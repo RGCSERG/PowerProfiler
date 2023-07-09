5533c27dfb3eb52b16cc1e46a214207167cc6ad3
RGCSERG <sergentrufus@gmail.com>
2023-07-09 19:17:17 +0100

    optimised oauth2 system operational with databse

--- fastapi-backend/refresh.py
+++ fastapi-backend/refresh.py
@@ -1,19 +1,53 @@
-from fastapi import FastAPI, HTTPException, Depends, Request
+from fastapi import Depends, FastAPI, HTTPException, status, Request
 from fastapi.responses import JSONResponse
 from fastapi_jwt_auth import AuthJWT
 from fastapi_jwt_auth.exceptions import AuthJWTException
 from pydantic import BaseModel
+from passlib.context import CryptContext
+from .database import conn, cursor
+from fastapi.encoders import jsonable_encoder
 
 app = FastAPI()
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 
 class User(BaseModel):
-    username: str
+    email: str
     password: str
 
 
 class Settings(BaseModel):
     authjwt_secret_key: str = "secret"
+
+
+def verify_password(plain_password, hashed_password):
+    return pwd_context.verify(plain_password, hashed_password)
+
+
+def get_password_hash(password):
+    return pwd_context.hash(password)
+
+
+def get_user(email: str):
+    cursor.execute("""SELECT * FROM public."Users" WHERE email = %s """, (email,))
+    user = cursor.fetchone()
+
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"User not found",
+        )
+    json_compatible_item_data = jsonable_encoder(user)
+    return User(**json_compatible_item_data)
+
+
+def authenticate_user(userData: User):
+    user = get_user(userData.email)
+    if not user:
+        return False
+    if not verify_password(userData.password, user.password):
+        return False
+    return user
 
 
 @AuthJWT.load_config
@@ -28,13 +62,11 @@ def authjwt_exception_handler(request: Request, exc: AuthJWTException):
 
 @app.post("/login")
 def login(user: User, Authorize: AuthJWT = Depends()):
-    if user.username != "test" or user.password != "test":
-        raise HTTPException(status_code=401, detail="Bad username or password")
-
+    authenticate_user(user)
     # Use create_access_token() and create_refresh_token() to create our
     # access and refresh tokens
-    access_token = Authorize.create_access_token(subject=user.username)
-    refresh_token = Authorize.create_refresh_token(subject=user.username)
+    access_token = Authorize.create_access_token(subject=user.email)
+    refresh_token = Authorize.create_refresh_token(subject=user.email)
     return {"access_token": access_token, "refresh_token": refresh_token}
 
 
